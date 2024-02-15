4315a20e139ee733fe7186e8953373a570dec3fc
Daljit Kaur <daljit_kaur@softprodigy.com>
2024-02-15 12:31:16 +0530

    Pagination and Filteration added to students list

--- Shared/ViewModels/GetStudentVM.cs
+++ Shared/ViewModels/GetStudentVM.cs
@@ -10,8 +10,9 @@
 
         public string Batch { get; set; } 
 
-        public string Degree { get; set; } 
+        public string Degree { get; set; }
 
+        public string FullName { get; set; }
         public string FirstName { get; set; }
 
         public string LastName { get; set; } 
